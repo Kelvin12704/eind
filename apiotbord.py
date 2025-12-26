d03ad0cdd917d937eedee83e19496d630651d495
Kelvin Chen <kelvin.chen@student.ap.be>
2025-12-26 15:53:05 +0100

    add code apiot

--- apiotbord.py
+++ apiotbord.py
@@ -1,1 +1,13 @@
-test
+from machine import Pin, ADC
+from time import sleep
+
+def led(n): #define led 
+    return Pin(n+1, Pin.OUT)
+
+def start_up(): #define startup 
+    for i in range(1, 8):
+        led(i).on()
+        sleep(0.1)
+    for i in range(1, 8):
+        led(i).off()
+        sleep(0.1)
