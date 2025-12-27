649d51587fabc4a3b4dedeff6e54369a3d27ea33
Kelvin Chen <kelvin.chen@student.ap.be>
2025-12-27 14:41:45 +0100

    add pot temp knop

--- apiotbord.py
+++ apiotbord.py
@@ -11,3 +11,25 @@ def start_up(): #define startup
     for i in range(1, 8):
         led(i).off()
         sleep(0.1)
+
+def potentio():
+    """Return the potentiometer value"""
+    potentiometer = ADC(28)
+    potentiometer_value = potentiometer.read_u16()/8000
+    return potentiometer_value
+
+def temperature():
+    """Return the temperature value"""
+    temperature_sensor = ADC(4)
+    temperature_value = temperature_sensor.read_u16()
+    volt = (3.3/65535)*temperature_value
+    temperature = 27 - (volt - 0.706)/0.001721
+    return round(temperature, 1)  
+ 
+def knop1():
+    knp1 = Pin(22, Pin.IN, pull=Pin.PULL_UP)
+    return knp1
+
+def knop2():
+    knp2 = Pin(10, Pin.IN, pull=Pin.PULL_UP)
+    return knp2
--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-test
+from apiotbord import led, start_up, potentio, temperature, knop1, knop2
+from time import sleep
+
+start_up()
+while True:
+    potValue = int(potentio()) 
+
+    knop1_value = knop1().value()
+    knop2_value = knop2().value()
+
+    if 3 <= potValue <= 8:
+        led(potValue).on()
+        sleep(0.1)
+        led(potValue).off()
+        sleep(0.1)
+        if potValue == 3 and knop1_value == 0:
+            while True:
+                for i in range(9):  #led 1 voor 1 aan
+                    led(i).on()
+                    sleep(0.1)
+            
+                for i in range(9): #led 1 voor 1 uit
+                    led(i).off()
+                    sleep(0.1)
+                if knop2().value():
+                    break
