02239dc511d8fc6fcc64d236f73ef2c7bf9c8947
Kelvin Chen <kelvin.chen@student.ap.be>
2025-12-28 13:47:22 +0100

    programma 1 en 2

--- main.py
+++ main.py
@@ -6,14 +6,42 @@ while True:
     potValue = int(potentio()) 
 
     knop1_value = knop1().value()
-    knop2_value = knop2().value()
 
-    if 3 <= potValue <= 8:
+
+    if 3 <= potValue <= 8: #limiteert de potentiemeter 
         led(potValue).on()
         sleep(0.1)
         led(potValue).off()
         sleep(0.1)
-        if potValue == 3 and knop1_value == 0:
+    
+        elif potValue == 3 and knop1_value == 0: #selecteren programma 1
+            while True:
+                # Stop direct als knop2 wordt ingedrukt
+                knop2_value = knop2().value()
+                if knop2_value == 0:
+                    break
+
+                # LED loopt vooruit van 1 naar 8
+                for i in range(1, 8 + 1):
+                    led(i).on()
+                    sleep(0.1)
+                    led(i).off()
+                    
+
+               # LED loopt achteruit van 8 naar 1
+                for i in reversed(range(1, 8 + 1)):
+                    led(i).on()
+                    sleep(0.1)
+                    led(i).off()
+
+        
+                if knop2_value == 0: #stopt programma
+                    break
+
+        
+                sleep(1)  # korte pauze tussen loops
+
+        if potValue == 4 and knop1_value == 0: #selecteren programma 2
             while True:
                 for i in range(9):  #led 1 voor 1 aan
                     led(i).on()
@@ -22,5 +50,7 @@ while True:
                 for i in range(9): #led 1 voor 1 uit
                     led(i).off()
                     sleep(0.1)
-                if knop2().value():
+
+                knop2_value = knop2().value()
+                if knop2_value == 0: #stopt programma
                     break
