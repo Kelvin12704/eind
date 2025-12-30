e3c841b498e4d2c1f56e12d41566efac09b47810
Kelvin Chen <kelvin.chen@student.ap.be>
2025-12-30 14:24:14 +0100

    programma 3 test

--- apiotbord.py
+++ apiotbord.py
@@ -1,16 +1,20 @@
 from machine import Pin, ADC
 from time import sleep
 
+
 def led(n): #define led 
     return Pin(n+1, Pin.OUT)
 
-def start_up(): #define startup 
-    for i in range(1, 8):
-        led(i).on()
+def start_up():
+    for _ in range(5):
+        for i in range(9):
+            led(i).on()
         sleep(0.1)
-    for i in range(1, 8):
-        led(i).off()
+
+        for i in range(9):
+            led(i).off()
         sleep(0.1)
+
 
 def potentio():
     """Return the potentiometer value"""
@@ -18,13 +22,15 @@ def potentio():
     potentiometer_value = potentiometer.read_u16()/8000
     return potentiometer_value
 
-def temperature():
-    """Return the temperature value"""
-    temperature_sensor = ADC(4)
-    temperature_value = temperature_sensor.read_u16()
-    volt = (3.3/65535)*temperature_value
-    temperature = 27 - (volt - 0.706)/0.001721
-    return round(temperature, 1)  
+
+
+def temp():
+    temp_sensor = ADC(4)
+    adc_value = temp_sensor.read_u16()
+    voltage = adc_value * 3.0 / 65535
+    return 27 - ((voltage - 0.706) / 0.001721)
+
+  
  
 def knop1():
     knp1 = Pin(22, Pin.IN, pull=Pin.PULL_UP)
@@ -33,3 +39,35 @@ def knop1():
 def knop2():
     knp2 = Pin(10, Pin.IN, pull=Pin.PULL_UP)
     return knp2
+
+def led1():
+    led1 = Pin(2, Pin.OUT)
+    return led1
+    
+def led2():
+    led2 = Pin(3, Pin.OUT)
+    return led2
+
+def led3():
+    led1 = Pin(4, Pin.OUT)
+    return led3
+
+def led4():
+    led4 = Pin(5, Pin.OUT)
+    return led4
+
+def led5():
+    led5 = Pin(6, Pin.OUT)
+    return led5
+
+def led6():
+    led6 = Pin(7, Pin.OUT)
+    return led6
+
+def led7():
+    led7 = Pin(8, Pin.OUT)
+    return led7
+
+def led8():
+    led8 = Pin(9, Pin.OUT)
+    return led8
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from apiotbord import led, start_up, potentio, temperature, knop1, knop2
+from apiotbord import led, start_up, potentio, temp, knop1, knop2, led1, led2, led3, led4, led5, led6, led7, led8
 from time import sleep
 
 start_up()
@@ -14,43 +14,84 @@ while True:
         led(potValue).off()
         sleep(0.1)
     
-        elif potValue == 3 and knop1_value == 0: #selecteren programma 1
-            while True:
-                # Stop direct als knop2 wordt ingedrukt
-                knop2_value = knop2().value()
-                if knop2_value == 0:
-                    break
+    if potValue == 3 and knop1_value == 0: #selecteren programma 1
+        while True:
+            # Stop direct als knop2 wordt ingedrukt
+            knop2_value = knop2().value()
+            if knop2_value == 0:
+                break
 
-                # LED loopt vooruit van 1 naar 8
-                for i in range(1, 8 + 1):
-                    led(i).on()
-                    sleep(0.1)
-                    led(i).off()
+            # LED loopt vooruit van 1 naar 8
+            for i in range(1, 8 + 1):
+                led(i).on()
+                sleep(0.1)
+                led(i).off()
                     
 
                # LED loopt achteruit van 8 naar 1
-                for i in reversed(range(1, 8 + 1)):
-                    led(i).on()
-                    sleep(0.1)
-                    led(i).off()
+            for i in reversed(range(1, 8 + 1)):
+                led(i).on()
+                sleep(0.1)
+                led(i).off()
 
         
-                if knop2_value == 0: #stopt programma
-                    break
+            if knop2_value == 0: #stopt programma
+                break
 
         
-                sleep(1)  # korte pauze tussen loops
+            sleep(1)  # korte pauze tussen loops
 
-        if potValue == 4 and knop1_value == 0: #selecteren programma 2
-            while True:
-                for i in range(9):  #led 1 voor 1 aan
-                    led(i).on()
-                    sleep(0.1)
+    elif potValue == 4 and knop1_value == 0: #selecteren programma 2
+        while True:
+            for i in range(9):  #led 1 voor 1 aan
+                led(i).on()
+                sleep(0.1)
+        
+            for i in range(9): #led 1 voor 1 uit
+                led(i).off()
+                sleep(0.1)
+
+            knop2_value = knop2().value()
+            if knop2_value == 0: #stopt programma
+                break
+    elif potValue == 5 and knop1_value == 0: #selecteren programma 3
+        while True:
+            temperature = temp()
+            print("Temp:", temperature)
+            sleep(1)
+            if temperature > -10 and temperature < 0: #tussen -10 en 0 graden gaat led 1 aan
+                led1.on()
+                sleep(0.1)
+                led1.off()
+            if temperature >= 0 and temperature < 5: #tussen 0 en 5 graden gaat led 2 aan 
+                led2.on()
+                sleep(0.1)
+                led2.off()
+            if temperature >= 5 and temperature < 10: #enz...
+                led3.on()
+                sleep(0.1)
+                led3.off()
+            if temperature >= 10 and temperature < 15:
+                led4.on()
+                sleep(0.1)
+                led4.off()
+            if temperature >= 15 and temperature < 20:
+                led5.on()
+                sleep(0.1)
+                led5.off()
+            if temperature >= 20 and temperature < 25:
+                led6.on()
+                sleep(0.1)
+                led6.off()
+            if temperature >= 25 and temperature < 30:
+                led7.on()
+                sleep(0.1)
+                led7.off()
+            if temperature >= 30 and temperature < 35:
+                led8.on()
+                sleep(0.1)
+                led8.off()
             
-                for i in range(9): #led 1 voor 1 uit
-                    led(i).off()
-                    sleep(0.1)
-
-                knop2_value = knop2().value()
-                if knop2_value == 0: #stopt programma
-                    break
+            knop2_value = knop2().value()
+            if knop2_value == 0: #stopt programma
+                break
